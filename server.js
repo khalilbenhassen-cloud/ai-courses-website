b714505479b0cb448ca881475c965c7996091a0e
khalilbenhassen-cloud <khalilbenhassen27@gmail.com>
2026-05-13 00:52:36 +0200

    update

--- server.js
+++ server.js
@@ -41,6 +41,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
 app.get('/course/:id', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'course.html'));
 });
